a302f7508bccef082d69f045ef04dc4b037776c0
Steve Zimmerman <szman86@gmail.com>
2015-01-12 15:22:25 -0800

    first release with routes to questions implemented

--- Gemfile
+++ Gemfile
@@ -13,6 +13,10 @@ gem 'uglifier', '>= 1.3.0'
 gem 'coffee-rails', '~> 4.0.0'
 # See https://github.com/sstephenson/execjs#readme for more supported runtimes
 # gem 'therubyracer',  platforms: :ruby
+gem 'rspec-rails'
+gem 'capybara'
+gem 'shoulda-matchers'
+gem 'factory_girl'
 
 # Use jquery as the JavaScript library
 gem 'jquery-rails'
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,13 @@
 Rails.application.routes.draw do
+  get 'welcome/index'
+
+  get 'questions/index'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  root 'questions#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
@@ -13,6 +17,9 @@ Rails.application.routes.draw do
 
   # Example resource route (maps HTTP verbs to controller actions automatically):
   #   resources :products
+
+  resources :questions
+
 
   # Example resource route with options:
   #   resources :products do
