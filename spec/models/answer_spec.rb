87b69bc858614e2d876ed1f3ac9cf766518ed3c1
Steve Zimmerman <szman86@gmail.com>
2015-01-13 10:41:55 -0800

    tweaked views

--- spec/models/answer_spec.rb
+++ spec/models/answer_spec.rb
@@ -1,5 +1,16 @@
 require 'rails_helper'
 
-RSpec.describe Answer, :type => :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+# RSpec.describe Answer, :type => :model do
+#   pending "add some examples to (or delete) #{__FILE__}"
+# end
+
+describe __ do
+  context "validations" do
+    it "title must be present" do
+      question = Question.new :not_title => "abc"
+      expect {
+        question.save
+      }.to_not change { Question.count }
+    end
+  end
 end
